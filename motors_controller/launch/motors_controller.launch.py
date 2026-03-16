90bd736506f2d2985894119a8ebf59fd03689a83
KauaOliveira11 <kauasilvaolivera998@gmail.com>
2026-03-16 16:34:31 -0300

    Organizando os pacotes separado, arrumando os launchs e adicionando o encontro automatico do arduino

---
+++ motors_controller/launch/motors_controller.launch.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_rviz = LaunchConfiguration('use_rviz', default='false')
+    rviz_config = LaunchConfiguration('rviz_config', default='')
+
+    cmd_vel_node = Node(
+        package='motors_controller',
+        executable='cmd_vel_to_serial',
+        name='cmd_vel_to_serial_ros2',
+        output='screen',
+        parameters=[
+            {'port': '/dev/ttyUSB0'},
+            {'baudrate': 115200}
+        ]
+
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(use_rviz)
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_rviz', default_value='false', description='Ativa RViz (true/false)'),
+        DeclareLaunchArgument('rviz_config', default_value='', description='Caminho para o arquivo de configuração do RViz'),
+        cmd_vel_node,
+        rviz_node
+    ])
---
+++ robot_sensors_publisher/launch/imu_sensor_publisher.launch.py
@@ -0,0 +1,38 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_rviz = LaunchConfiguration('use_rviz', default='false')
+    rviz_config = LaunchConfiguration('rviz_config', default='')
+
+    imu_node = Node(
+        package='robot_sensors_publisher',
+        executable='imu_serial_publisher',
+        name='imu_serial_publisher',
+        output='screen',
+        parameters=[
+            {'port': '/dev/ttyUSB0'},
+            {'baudrate': 115200}
+        ]
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(use_rviz)
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument('use_rviz', default_value='false', description='Ativa RViz (true/false)'),
+        DeclareLaunchArgument('rviz_config', default_value='', description='Caminho para o arquivo de configuração do RViz'),
+        imu_node,
+        rviz_node
+    ])
